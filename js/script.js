ce4281526b1a7275bac7fab61c9fb44abf47b9ca
Willem A. Smit <wianwasmit@gmail.com>
2016-10-10 16:28:13 +0200

    Plain program working

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 $(document).ready( function () {
-	makeGrid();
+	makeGrid(32);
 	$('.row>div').mouseenter( function () {
 		$(this).addClass('active')
 	})
-	$('.row>div').mouseleave( function () {
-		$(this).removeClass('active')
-	})
 });
 
-var makeGrid = function () {
-	for (var i = 0; i < 16; i++) {
+var makeGrid = function (number) {
+	dist = 600/number
+	for (var i = 0; i < number; i++) {
 		$('.wrapper').append("<div class = 'row'></div>");
-		for (var j = 0; j < 16; j++) {
+		/*$('.wrapper:last-child').width(600)
+		$('.wrapper:last-child').height(Math.round(dist-0.5))*/
+		for (var j = 0; j < number; j++) {
 			$('.wrapper>div:last-child').append("<div></div>");
+			$('.wrapper>div>div:last-child').width(Math.round(dist-0.5))
+			$('.wrapper>div>div:last-child').height(Math.round(dist-0.5))
 		};
 	};
+}
+
+function refresh() {
+	$('.row>div').removeClass('active')
 }
